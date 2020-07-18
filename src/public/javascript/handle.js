48305b1f258d2d015c4800d0e1e97af913776412
truongtrunghieu <truongtrung.hieu0812@gmail.com>
2020-07-19 00:37:48 +0700

    update

--- src/public/javascript/handle.js
+++ src/public/javascript/handle.js
@@ -13,7 +13,7 @@ let handle_cookie = (cookie) => {
         let list = document.querySelectorAll('.item');
         for (var i = 0; i < list.length; ++i) {
             list[i].classList.add('item-revert');
-         }
+        }
         setTimeout(() => {
             document.cookie = "dark";
         }, 1000)
@@ -26,7 +26,7 @@ let handle_cookie = (cookie) => {
         let list = document.querySelectorAll('.item');
         for (var i = 0; i < list.length; ++i) {
             list[i].classList.remove('item-revert');
-         }
+        }
         setTimeout(() => {
             document.cookie = "white";
         }, 1000)
@@ -50,3 +50,55 @@ document.onkeydown = function (event) {
         return false;
     }
 }
+
+// SWIPE
+
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;
+var yDown = null;
+
+function getTouches(evt) {
+    return evt.touches ||             // browser API
+        evt.originalEvent.touches; // jQuery
+}
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+};
+
+function handleTouchMove(evt) {
+    if (!xDown || !yDown) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {/*most significant*/
+        if (xDiff > 0) {
+            let cookie = document.cookie;
+            handle_cookie(cookie)
+        } else {
+            let cookie = document.cookie;
+            handle_cookie(cookie)
+        }
+    } else {
+        if (yDiff > 0) {
+            let cookie = document.cookie;
+            handle_cookie(cookie)
+        } else {
+            let cookie = document.cookie;
+            handle_cookie(cookie)
+        }
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;
+};
